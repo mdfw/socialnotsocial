7fb2fc9e02fd2e1779bbc38e700a672fd4b2d723
Mark Williams <github@shymedia.com>
2017-01-09 15:13:35 -0800

    added config files

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,23 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/js/index.js'),
-    output: {
-        path: path.resolve(__dirname, 'build/js'),
-        filename: 'index.js',
-    },
-    devtool: 'inline-source-map',
-    module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          exclude: /(node_modules)/,
-          loader: 'babel',
-        },
-      ]
-    }
+  entry: path.resolve(__dirname, 'src/js/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build/js'),
+    filename: 'index.js',
+  },
+  resolve: {
+    modulesDirectories: ['node_modules', 'shared'],
+    extensions:         ['', '.js', '.jsx']
+  },    
+  devtool: 'inline-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+      },
+    ]
+  }
 };
