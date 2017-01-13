059603b0ce7b80db50250afa043dc881ae679662
Mark Williams <github@shymedia.com>
2017-01-13 13:42:44 -0800

    Added js to jsx

--- webpack.apiserver.config.js
+++ webpack.apiserver.config.js
@@ -28,7 +28,7 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /.jsx?$/,
+        test: /.(jsx|js)?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
