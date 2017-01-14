7ead04e9d3c20397dfd40e822d7bbc08e14f9855
Mark Williams <github@shymedia.com>
2017-01-14 11:25:33 -0800

    Switched to the global promise library since the promise library in mongoose is deprecated.

--- src/config/mongoConnect.js
+++ src/config/mongoConnect.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 
 const MONGO_URL = process.env.MONGO_URL;
 
+mongoose.Promise = global.Promise;
+
 /* Connect to mongo */
 mongoose.connect(MONGO_URL);
 mongoose.connection
