82257af4fe01401b06aa3c0505568272b0c56d92
Mark Williams <github@shymedia.com>
2017-01-14 11:42:37 -0800

    added documentation

--- src/shared/helpers/signupValidators.js
+++ src/shared/helpers/signupValidators.js
@@ -1,5 +1,8 @@
 import { isEmail } from 'validator';
 
+/* Validates various fields that are required on new accounts. 
+ * Provides flexible tests depending on needs.
+ */
 class SignupValidator {
   constructor(email, password, displayName) {
     console.log(`validator: email: ${email} password: ${password} displayName: ${displayName}`);
@@ -8,6 +11,9 @@ class SignupValidator {
     this.displayName = displayName;
   }
   allFields() {
+    /* All fields will test all fields as though they were required. Runs tests and returns
+     * the validator object with the errors attached to the errors object.
+     */
     const validator = {
       success: true,
       errors: {
@@ -35,8 +41,11 @@ class SignupValidator {
     return validator;
   }
   displayNameValid() {
+    /* displayNameValid tests for the validity of the displayName.
+     *  Only checks if it's empty.
+     */
     const messages = [];
-    if (!this.displayName) {
+    if (!this.displayName.trim()) {
       messages.push('Display name is required.');
     }
     if (messages.length > 0) {
@@ -45,8 +54,10 @@ class SignupValidator {
     return null;
   }
   emailValid() {
+    /* Is the email valid? Uses the validator library to test.
+     */
     const messages = [];
-    if (!this.email) {
+    if (!this.email.trim()) {
       messages.push('Email address is required.')
     }
     if (this.email && !isEmail(this.email)) {
@@ -58,8 +69,11 @@ class SignupValidator {
     return null;
   }
   passwordValid() {
+    /* Currently only checks on empty.
+     *  TODO: add other checks for a valid password.
+     */
     const messages = [];
-    if (!this.password) {
+    if (!this.password.trim()) {
       messages.push('Password is required.');
     }
     if (messages.length > 0) {
