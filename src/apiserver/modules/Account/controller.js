4a421e39433b2bf4454f022cb669da8361ee9ebe
Mark Williams <github@shymedia.com>
2017-01-14 12:13:52 -0800

    added the AddAccount method

--- src/apiserver/modules/Account/controller.js
+++ src/apiserver/modules/Account/controller.js
@@ -2,10 +2,10 @@ import Account from './model';
 import SignupValidator from '../../../shared/helpers/signupValidators';
 
 export const signup = (req, res) => {
-  console.log('got to signup with body: ');
-  console.dir(req.body);
+  /* Signing up requires an email, password and displayName.
+   * Checks for validity of these fields uing the signupValidators.
+   */
   const { email, password, displayName } = req.body;
-  console.log(`controller: email: ${email} password: ${password} displayName: ${displayName}`);
 
   /* Validate the fields */
   const validator = new SignupValidator(email, password, displayName);
@@ -16,14 +16,14 @@ export const signup = (req, res) => {
 
   // Search the database for a user object with the submitted email.
   Account.findOne({ 'local.email': email })
-    .then((auth) => {
-      if (auth) res.status(422).json({ success: false, message: 'Email is already in use.' });
+    .then((account) => {
+      if (account) res.status(422).json({ success: false, message: 'Email is already in use.' });
 
-      // Create a new instance of the user model for the new user.
+      // Create a new instance of the account model.
       const newAccount = new Account({ local: { email, password, displayName } });
-      // Save the new user object to the database.
+      // Save the new account object to the database.
       newAccount.save()
-        .then(account => res.status(201).json({
+        .then(() => res.status(201).json({
           success: true,
           message: 'Successfully Registerd',
         }))
@@ -33,4 +33,37 @@ export const signup = (req, res) => {
           res.status(422).json({ success: false, message: error || err });
         });
     });
+};
+
+export const addAccount = (email, displayName) => {
+  const validator = new SignupValidator(email, null, displayName);
+
+  const emailValidationMessages = validator.displayNameValid();
+  if (emailValidationMessages.length > 0) {
+    return new Error(emailValidationMessages.join());
+  }
+  const displayNameValidationMessages = validator.emailValid();
+  if (displayNameValidationMessages.length > 0) {
+    return new Error(displayNameValidationMessages.join());
+  }
+  Account.findOne({ 'local.email': email })
+  .then((dupaccount) => {
+    if (dupaccount) {
+      return dupaccount;
+    }
+
+    // Create a new instance of the account model.
+    const newAccount = new Account({ local: { email, displayName } });
+    // Save the new account object to the database.
+    newAccount.save()
+      .then((account) => { return account; })  // eslint-disable-line arrow-body-style
+      .catch((err) => {
+        let error;
+        if (err.code === 11000) error = 'Email is already in use.';
+        const errormsg = error || err;
+        return new Error(errormsg);
+      });
+    return null;
+  });
+  return null;
 };
