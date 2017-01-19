481c9ed9f6a1e6d8ff0520953e7debbd27d62211
Mark Williams <github@shymedia.com>
2017-01-18 20:35:01 -0800

    Removed debugging codes

--- src/apiserver/modules/Account/apiController.js
+++ src/apiserver/modules/Account/apiController.js
@@ -5,22 +5,6 @@ const addAccountEndpoint = (req, res) => {
   const newAccount = new Account({ email, password, displayName });
   console.log('Heres the new account before save or even setting password');
   console.dir(newAccount);
-        console.log('newAccount has setPassword?');
-      if(typeof newAccount.setPassword === 'function') {
-        console.log("It's a function");
-      } else if (typeof newAccount.setPassword === 'undefined') {
-        console.log("It's undefined");
-      } else {
-        console.log("It's neither undefined nor a function. It's a " + typeof newAccount.setPassword);
-      }
-      console.log('newAccount has comparePassword?');
-      if(typeof newAccount.comparePassword === 'function') {
-        console.log("It's a function");
-      } else if (typeof newAccount.comparePassword === 'undefined') {
-        console.log("It's undefined");
-      } else {
-        console.log("It's neither undefined nor a function. It's a " + typeof newAccount.comparePassword);
-      }
   newAccount.setPassword(password)
     .then(() => { // eslint-disable-line arrow-body-style
       return newAccount.save();
