c0b702f6bdac230079515092ce85a0191c87e9f9
Mark Williams <github@shymedia.com>
2017-01-29 19:49:38 -0800

    Added a settings object for name, colors and other sitewide switches.

---
+++ src/shared/settings.js
@@ -0,0 +1,12 @@
+const settings = {
+  baseColor: '#459691',
+  alertColor: '#c94f49',
+  highlightColor: '#d9ecfc',
+  greenerColor: '#43a366',
+  blueColor: '#4376a3',
+  lightBlueColor: '#d9ecfc',
+  siteName: 'Social, Not Social',
+  postMaxCharacters: '5000',
+};
+
+export default settings;
