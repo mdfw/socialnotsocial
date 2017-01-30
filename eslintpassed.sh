e1df7ff7d19ee046d4ab2e6c37754b4fb0556c91
Mark Williams <github@shymedia.com>
2017-01-30 11:02:53 -0800

    Removed the linting script since we aren't using it

--- eslintpassed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash          
-linted = `./node_modules/eslint/bin/eslint.js ./src --ext .js --ext .jsx --cache`
-
-if $linted then 
-  echo $linted
-else 
-  echo "$(tput setaf 1)Red text $(tput setab 7)and white background$(tput sgr 0)"
-fi
