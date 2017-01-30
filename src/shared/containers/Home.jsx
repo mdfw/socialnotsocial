5b28a76302fe06b655465747f159b507c9e2ec97
Mark Williams <github@shymedia.com>
2017-01-29 19:50:22 -0800

    Removed the Paper outline here and moved it to the post form controller so we could style it

--- src/shared/containers/Home.jsx
+++ src/shared/containers/Home.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import Paper from 'material-ui/Paper';
 import CreatePostForm from './CreatePostForm';
 import Posts from './Posts';
-
-const createPostPaperStyle = {
-  padding: '15px',
-};
 
 /* This is the component that holds the posts and actions for the post.
  *    Similar to a 'wall'
  */
 const Home = () => (
   <div id="board" className="board">
-    <Paper zDepth={2} style={createPostPaperStyle}>
-      <CreatePostForm />
-    </Paper>
+    <CreatePostForm />
     <Posts />
   </div>
 );
