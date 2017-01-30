24271dfc33bdf1f3aaac1172bdbd8e064b36d5fe
Mark Williams <github@shymedia.com>
2017-01-30 10:09:22 -0800

    Will not load dotenv in production and adds validation for the required environment variables

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,25 +1,27 @@
 import dotenv from 'dotenv';
+import requireEnv from 'require-environment-variables';
 
 /* 'As early as possible in your application, require and configure dotenv.'
  *   - https://www.npmjs.com/package/dotenv
+ *
+ * However, we aren't going to load these in production as it could lead to sloppy deploys.
  */
-dotenv.config();
+if (process.env.NODE_ENV && process.env.NODE_ENV !== 'production') {
+  dotenv.load();
+} else {
+  dotenv.load();
+}
 
-// Bring in Envvars from .env.
-const envVars = [
+/* If any of the environment variables(process.env.REDIS_URL) don't exist,
+ *   the process exits with code 400.
+ * https://github.com/bjoshuanoah/require-environment-variables
+ */
+requireEnv([
   'REDIS_URL',
   'MONGODB_URI',
-  'NODE_ENV',
   'API_SERVER_PORT',
   'MAIN_SERVER_PORT',
   'ACCOUNT_PEPPER_1',
   'ACCOUNT_ENCRYPT_CURRENT_PEPPER',
   'IDIER_WORKER_ID',
-];
-
-// Check that Envvars are set.
-envVars.forEach((env) => {
-  if (!process.env[env]) {
-    throw new Error(`Environment variable ${env} not set.`);
-  }
-});
+]);
