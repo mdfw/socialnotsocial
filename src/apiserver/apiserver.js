634338437b4bba5ccd6123f6afee685365f2e0fd
Mark Williams <github@shymedia.com>
2017-02-13 09:38:18 -0800

    Add session_secret, token_pepper information to environment. Use session_secret instead of hard coded

--- src/apiserver/apiserver.js
+++ src/apiserver/apiserver.js
@@ -33,7 +33,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
   name: 'snss',
-  secret: 'MmyWTLNNsTi15LYHz8FP',
+  secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: false,
   store: new RedisStore({ client: redisClient }),
