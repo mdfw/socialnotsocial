dbf9b80b16ab45b1d23a490d405a325b0cbabcf8
Mark Williams <github@shymedia.com>
2017-02-27 12:24:56 -0800

    Update textField to allow require and change text sizes

--- src/shared/components/ui/TextField.jsx
+++ src/shared/components/ui/TextField.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
+const TextSize = {
+  EXLARGE: 0,
+  LARGE: 1,
+  MEDIUM: 2,
+  SMALL: 3,
+  selector: ['sns-text-field-exlg', 'sns-text-field-lg', 'sns-text-field-md', 'sns-text-field-sm'],
+};
+
 const TextField = (props) => {
   let inputDisabled = false;
   if (props.disabled) {
     inputDisabled = true;
+  }
+  let inputRequired = false;
+  if (props.required) {
+    inputRequired = true;
   }
   const errorName = `${props.name}-error`;
   let hasErrors = false;
@@ -15,6 +27,17 @@ const TextField = (props) => {
   if (props.type) {
     inputType = props.type;
   }
+  let placeholder = '';
+  if (props.hintText && props.hintText.length > 0) {
+    placeholder = props.hintText;
+  }
+  const inputSelectors = ['sns-text-field-input', TextSize.selector[TextSize.LARGE]];
+  if (props.textSize) {
+    const newTextSize = TextSize.selector[props.textSize];
+    if (newTextSize) {
+      inputSelectors[1] = newTextSize;
+    }
+  }
   return (
     <div className="sns-text-field">
       <div className="sns-text-field-title-holder">
@@ -22,13 +45,14 @@ const TextField = (props) => {
       </div>
       <div className="sns-text-field-input-holder">
         <input
-          className="sns-text-field-input"
+          className={inputSelectors.join(' ')}
           name={props.name}
           disabled={inputDisabled}
+          placeholder={placeholder}
           type={inputType}
           value={props.value}
           aria-label={props.labelText}
-          aria-required={true}
+          aria-required={inputRequired}
           aria-describedby={errorName}
           aria-invalid={hasErrors}
           onChange={props.onChange}
@@ -44,13 +68,21 @@ const TextField = (props) => {
 TextField.propTypes = {
   name: React.PropTypes.string.isRequired,
   labelText: React.PropTypes.string.isRequired,
-  disabled: React.PropTypes.bool,
-  type: React.PropTypes.string,
+  hintText: React.PropTypes.string,
+  required: React.PropTypes.bool, // Default false
+  disabled: React.PropTypes.bool, // Default false
+  type: React.PropTypes.string, // Default 'text'
   value: React.PropTypes.string,
+  textSize: React.PropTypes.oneOf([ // Defaults to TextSize.LARGE
+    TextSize.EXLARGE,
+    TextSize.LARGE,
+    TextSize.MEDIUM,
+    TextSize.SMALL,
+  ]),
+  errorText: React.PropTypes.string,
   onChange: React.PropTypes.func,
   onBlur: React.PropTypes.func,
   onFocus: React.PropTypes.func,
-  errorText: React.PropTypes.string,
 };
 
-export default TextField;
+export { TextField, TextSize };
