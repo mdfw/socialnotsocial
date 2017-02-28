d4cecc60d993d0c63180cdb4e97813fd6a912950
Mark Williams <github@shymedia.com>
2017-02-27 23:16:14 -0800

    Add getAPostEndpoint

--- src/apiserver/modules/Post/routes.js
+++ src/apiserver/modules/Post/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import {
   getPostsEndpoint,
+  getAPostEndpoint,
   addPostEndpoint,
   updatePostEndpoint,
   removePostEndpoint,
@@ -9,6 +10,7 @@ import { requireLogin } from '../Authentication/warrant';
 
 const routes = new Router();
 routes.get('/posts', requireLogin(), getPostsEndpoint);
+routes.get('/posts/:postId', requireLogin(), getAPostEndpoint);
 routes.post('/posts', requireLogin(), addPostEndpoint);
 routes.put('/posts/:postId', requireLogin(), updatePostEndpoint);
 routes.delete('/posts/:postId', requireLogin(), removePostEndpoint);
